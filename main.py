a400fd522ff1d5fccfb3ac7698152dea32d61755
Paolo Palmiero <paolopalmiero@Paolos-MacBook-Air.local>
2024-10-16 18:44:17 +0200

    Added the cumulative sums of data

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ def somma_cumulata(_var):
     somma = 0
     for i in range(0, len(_var)):
         somma += _var[i]
-        cumulata.append(somma)
-    return cumulata
+        #cumulata.append(somma)
+    #return cumulata
+    return somma
 
 def coeff(_nominalValue, _varCoeff): #Ai, A'i, Ditot, Eipot, Eiirr, Eiind
     coeffValue = float(_nominalValue) * float(_varCoeff)
@@ -26,8 +27,12 @@ def form():
     if request.method == 'POST':
         vol = ["S", "Winv tot", "Winv aut", "Wo", "A", "A'", "P ev", "P inf", "D ec", "E pot", "E irr", "E ind"]
         keys = ["Cj(A)", "Cj(A')", "Cj(ev)", "Cj(inf)", "Cj(ec)", "Cj(pot)", "Cj(irr)", "Cj(ind)"]
-        out = ["A", "A'", "P ev", "P inf", "D ec", "E pot", "E irr", "E ind"]
+        outj = ["A", "A'", "P ev", "P inf", "D ec", "E pot", "E irr", "E ind"]
+        out = ["A*", "A'*", "P ev*", "P inf*", "D ec*", "E pot*", "E irr*", "E ind*"]
         data = {}
+        values_tot = []
+        values_aitot = []
+        values_wi = []
 
         data["Mese di partenza"] = request.form.get('starting_month')
         
@@ -36,16 +41,35 @@ def form():
 
         for k in keys:
             values = []
-            values_coeff = []
             for i in range(1, 13):
                 values.append(request.form.get(f'coeff-{i}-{keys.index(k) + 1}'))
             data[k] = values
 
-        for k, o in zip(keys, out):
+        for k, o in zip(keys, outj):
             values = []
             for i in range(0, 12):
                 values.append(coeff(data[o], data[k][i]))
             data[o + " j"] = values
+
+        for o, oj in zip(out, outj):
+            data[o] = somma_cumulata(data[oj + " j"])
+
+        for i in range(0, 12):
+            values_tot.append(data["D ec j"][i] + data["E pot j"][i] + data["E irr j"][i] + data["E ind j"][i])
+        data["Etot j"] = values_tot
+        data["Etot*"] = somma_cumulata(data["Etot j"])
+
+        for i in range(0, 12):
+            values_aitot.append(data["A j"][i] + data["A' j"][i])
+        data["Aitot j"] = values_aitot
+        data["Aitot*"] = somma_cumulata(data["Aitot j"])
+
+        for i in range(0, 12):
+            values_wi.append(data["Aitot j"][i] - data["Etot j"][i])
+        data["w j"] = values_wi
+        data["w*"] = somma_cumulata(data["w j"])
+
+
 
         with open('data.json', 'w') as json_file:
             json.dump(data, json_file, indent=4)
