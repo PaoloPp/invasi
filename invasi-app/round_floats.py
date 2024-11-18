38989596234260766f126d63fb1ac7ec9010d4be
Paolo Palmiero <paolopalmiero@Paolos-MacBook-Air.local>
2024-11-18 17:56:54 +0100

    Removed unnecessary files

--- invasi-app/round_floats.py
+++
@@ -1,9 +0,0 @@
-def round_floats(obj):
-    if isinstance(obj, dict):
-        return {key: round_floats(value) for key, value in obj.items()}
-    elif isinstance(obj, list):
-        return [round_floats(element) for element in obj]
-    elif isinstance(obj, float):
-        return round(obj, 2)
-    else:
-        return obj
